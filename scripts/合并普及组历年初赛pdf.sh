7383e2a0e0649043149c3d2ad78c838765aed864
rainboy <rainboylvx@qq.com>
2021-07-25 23:31:48 +0800

    update

--- scripts/合并普及组历年初赛pdf.sh
+++ scripts/合并普及组历年初赛pdf.sh
@@ -1,61 +1,24 @@
-../CSP-J_S
-│   ├── 2019
-│   │   ├── 2019-CCF-CSP-J1（C++）Ans.jpg
-│   │   ├── 2019-CCF-CSP-J1（C++）hk.pdf
-│   │   ├── 2019-CCF-CSP-J1（C++）.pdf
-│   │   ├── 2019-CCF-CSP-S1（C++）Ans.pdf
-│   │   └── 2019-CCF-CSP-S1（C++）.pdf
-│   └── 2020
-│       ├── CSP 2020-J_S 第一轮、第二轮；NOIP 2020 在线模拟赛；NOIP 2020 正式赛.pdf
-│       └── csp2020.pdf
-└── Round2
-    ├── 2019
-    │   ├── 2019-CCF-CSP-J2.pdf
-    │   ├── 2019-CCF-CSP-S2-day1.pdf
-    │   ├── 2019-CCF-CSP-S2-day2.pdf
-    │   ├── CSP-J-data.7z
-    │   ├── CSP-S-day1-data.7z
-    │   └── CSP-S-day2-data.7z
-    └── 2020
-        ├── CSP 2020-J_S 第一轮、第二轮；NOIP 2020 在线模拟赛；NOIP 2020 正式赛.pdf
-        ├── CSP-J_Data.7z
-        ├── CSP-S-call.7z
-        ├── CSP-S-julian.7z
-        ├── CSP-S-snakes.7z
-        └── CSP-S-zoo.7z
-
-../NOIP/Preliminary/2006/NOIP2006PreliminaryJunior(C++)Answer.pdf
-../NOIP/Preliminary/2006/NOIP2006PreliminaryJunior(C++)TestPaper.pdf
-../NOIP/Preliminary/2007/NOIP2007PreliminaryJunior(C++)Answer.doc
-../NOIP/Preliminary/2007/NOIP2007PreliminaryJunior(C++)TestPaper.pdf
-../NOIP/Preliminary/2010/NOIP2010Junior(C++)Answer.doc
-../NOIP/Preliminary/2011/noip2011hj_cpp.pdf
-../NOIP/Preliminary/2011/noip2011hj_sol_cpp.pdf
-../NOIP/Preliminary/2011/noip2011hs_cpp.pdf
-../NOIP/Preliminary/2011/noip2011hs_sol_cpp.pdf
-../NOIP/Preliminary/2012/noip2012hj_cpp.pdf
-../NOIP/Preliminary/2012/noip2012hj_sol.pdf
-../NOIP/Preliminary/2012/noip2012hs_cpp.pdf
-../NOIP/Preliminary/2012/noip2012hs_sol.pdf
-../NOIP/Preliminary/2013/noip2013hj_cpp.pdf
-../NOIP/Preliminary/2013/noip2013hj_sol.pdf
-../NOIP/Preliminary/2013/noip2013hs_cpp.pdf
-../NOIP/Preliminary/2013/noip2013hs_sol.pdf
-../NOIP/Preliminary/2014/noip2014hj_cpp.pdf
-../NOIP/Preliminary/2014/noip2014hj_sol.pdf
-../NOIP/Preliminary/2014/noip2014hs_cpp.pdf
-../NOIP/Preliminary/2014/noip2014hs_sol.pdf
-../NOIP/Preliminary/2015/noip2015hj_cpp.pdf
-../NOIP/Preliminary/2015/noip2015hj_sol.pdf
-../NOIP/Preliminary/2015/noip2015hs_cpp.pdf
-../NOIP/Preliminary/2015/noip2015hs_sol.pdf
-../NOIP/Preliminary/2016/noip2016hj_cpp.pdf
-../NOIP/Preliminary/2016/noip2016hj_sol.pdf
-../NOIP/Preliminary/2016/noip2016hs_cpp.pdf
-../NOIP/Preliminary/2016/noip2016hs_sol.pdf
-../NOIP/Preliminary/2017/noip2017hj_cpp.pdf
-../NOIP/Preliminary/2017/noip2017hj_sol.pdf
-../NOIP/Preliminary/2017/noip2017hs_cpp.pdf
-../NOIP/Preliminary/2017/noip2017hs_sol.pdf
-../NOIP/Preliminary/2018/NOIP2018PreliminaryJuniorAnswer.pdf
-../NOIP/Preliminary/2018/NOIP2018PreliminaryJunior(C++)TestPaper.pdf
+pdfunite "../NOIP/Preliminary/2009/NOIP2009PreliminaryJunior(C++).pdf" \
+"../NOIP/Preliminary/2009/NOIP2009PreliminaryJunior(C++)Answer.pdf" \
+"../NOIP/Preliminary/2010/NOIP2010第十六届提高组初赛题目C++.pdf" \
+"../NOIP/Preliminary/2010/NOIP2010Junior(C++)Answer.pdf" \
+"../NOIP/Preliminary/2011/noip2011hj_cpp.pdf" \
+"../NOIP/Preliminary/2011/noip2011hj_sol_cpp.pdf" \
+"../NOIP/Preliminary/2012/noip2012hj_cpp.pdf" \
+"../NOIP/Preliminary/2012/noip2012hj_sol.pdf" \
+"../NOIP/Preliminary/2013/noip2013hj_cpp.pdf" \
+"../NOIP/Preliminary/2013/noip2013hj_sol.pdf" \
+"../NOIP/Preliminary/2014/noip2014hj_cpp.pdf" \
+"../NOIP/Preliminary/2014/noip2014hj_sol.pdf" \
+"../NOIP/Preliminary/2015/noip2015hj_cpp.pdf" \
+"../NOIP/Preliminary/2015/noip2015hj_sol.pdf" \
+"../NOIP/Preliminary/2016/noip2016hj_cpp.pdf" \
+"../NOIP/Preliminary/2016/noip2016hj_sol.pdf" \
+"../NOIP/Preliminary/2017/noip2017hj_cpp.pdf" \
+"../NOIP/Preliminary/2017/noip2017hj_sol.pdf" \
+"../NOIP/Preliminary/2018/NOIP2018PreliminaryJunior(C++)TestPaper.pdf" \
+"../NOIP/Preliminary/2018/NOIP2018PreliminaryJuniorAnswer.pdf" \
+"../CSP-J_S/Round1/2019/2019-CCF-CSP-J1（C++）.pdf" \
+"../CSP-J_S/Round1/2019/2019-CCF-CSP-J1（C++）Ans.pdf" \
+"../CSP-J_S/Round1/2020/csp2020.pdf" \
+普及组历年初赛题目合集.merge.pdf
